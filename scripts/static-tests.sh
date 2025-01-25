34820c8b1628cf879cffe23e318452b65f6eab88
alastair-h <ajwhirst@gmail.com>
2025-01-25 18:26:52 +0100

    ci improvements

--- scripts/static-tests.sh
+++ scripts/static-tests.sh
@@ -1,2 +1,2 @@
-set -ex
-docker-compose -f docker-compose.yaml run --rm api black .  --check 
+docker-compose -f docker-compose.yaml run --rm api black . --check
+docker-compose -f docker-compose.yaml run --rm api isort .  --check-only --diff
